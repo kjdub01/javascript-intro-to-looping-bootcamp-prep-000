44041fc5f6234ded9cbcbf5023f7f48177d10c08
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 21:04:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i<25; i++){
-    if (i === 1) return console.log("I am 1 strange loop");
-    else return ("I am $(i) strange loops");
+    if (i === 0) return console.log("I am $(i) strange loops");
+    else return ("I am 1 strange loop");
   }
 }
